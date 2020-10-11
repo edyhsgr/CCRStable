1413bae7d57096cee4f67f47c7cf6bff3fef8501
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2020-10-11 12:22:25 -0700

    Add files via upload

--- CCRStable_ValView_Florida.R
+++ CCRStable_ValView_Florida.R
@@ -165,8 +165,8 @@ options = list(placeholder = "Type in a county to see graphs", multiple = TRUE, 
                   ),
       ),
       
-      numericInput("BAStart","If yes, Brass' model alpha for First projection step...",.03,-2,2,step=.03),
-      numericInput("BAEnd","...and Brass' model alpha for Last projection step",.12,-2,2,step=.03),
+      numericInput("BAStart","If yes, Brass' model alpha for First projection step...",-.03,-2,2,step=.03),
+      numericInput("BAEnd","...and Brass' model alpha for Last projection step",.06,-2,2,step=.03),
 
       selectInput("LifeTable", "Life table standard to use", selected="Florida",
                   c(
